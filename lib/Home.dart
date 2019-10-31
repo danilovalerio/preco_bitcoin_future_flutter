abc9aecfbf27a6c3d208eb99125b09ef71d2c90e
dvalerio <dnotms290>
2019-10-31 13:09:52 -0300

    Consumindo API com Future e exibindo um "loading" enquanto não obtém os dados.

--- lib/Home.dart
+++ lib/Home.dart
@@ -9,8 +9,47 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  Future<Map> _recuperarPreco() async {
+    //url da api
+    String url = "https://blockchain.info/ticker";
+
+    Response response;
+    response = await get(url);
+
+    return json.decode(response.body);
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return  FutureBuilder<Map>(
+      future: _recuperarPreco(),
+      builder: (context, snapshot){
+
+        String resutado;
+        switch(snapshot.connectionState){
+          case ConnectionState.none :
+          case ConnectionState.waiting:
+            print("Conexão waiting");
+            resutado = "Carregando...";
+            break;
+          case ConnectionState.active:
+          case ConnectionState.done:
+            print("Conexão done");
+            if(snapshot.hasError){
+              resutado = "Erro ao carregar os dados.";
+            } else {
+              double valor = snapshot.data["BRL"]["buy"];
+              resutado = "Preço do bitcoin ${valor.toString()}";
+            }
+            break;
+        }
+
+        return Center(
+          child: Text(resutado),
+        );
+      },
+    );
   }
 }
